42c2df8e3c1edad54002fa3395fc67ee2daba6ec
Afriatsya <153706658+Afriatsya@users.noreply.github.com>
2023-12-17 20:40:49 +0800

    upload file 6

---
+++ praktikum-d6.py
@@ -0,0 +1,20 @@
+a = True
+
+while a:
+    jawab = input('apakah ingin lanjutkan? (y/n)')
+    if jawab == 'y':
+        print('terima kasih')
+        a = True
+    elif jawab == 'n':
+        print('sampai jumpa :D')
+        a = False
+    else:
+        print('jangan aneh deh:)')
+        a = True
+
+
+
+
+
+
+
