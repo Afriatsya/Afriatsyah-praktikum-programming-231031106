585345a03de71e7dbca68e20bca1fdeae54d0ff2
Afriatsya <153706658+Afriatsya@users.noreply.github.com>
2023-12-17 20:39:11 +0800

    upload file 5

---
+++ praktikum-d5.py
@@ -0,0 +1,53 @@
+a = True
+i = 0
+limit = 10
+
+while a:
+    i +=1          #i+1
+    if i <= limit:
+        print('selamat bergabung',limit + 1 -i)
+    else:
+        print('program berhenti di sini !!!!!!!')
+        a = False 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
