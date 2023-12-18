794ff505b298019385823a89369ebcbef0215062
Afriatsya <153706658+Afriatsya@users.noreply.github.com>
2023-12-18 20:07:39 +0800

    upload file 9

---
+++ 231031106-Afriatsyah Nasra Perdana-tugas dictionary-9-D.py
@@ -0,0 +1,14 @@
+print('\n')
+mhs = {'Nama'    : 'Afriatsyah Nasra Perdana',
+       'lulus'   : 'true',
+       'Status'  : 'Mahasiswa',
+       'Angkatan': '2023',
+       'prodi'   : 'Sistem Informasi',
+       'Nim'     : '231031106',
+       'asal SMA': 'SMAN 4 PAREPARE',
+       'matkul'  : {'mk1' : 'sains terpadu',
+                    'mk2' : 'pancasila',
+                    'mk3' : 'kalkulus dasar'}}
+print(mhs['Nama'])    
+print(mhs['prodi'])
+print(mhs['Nim'])    
