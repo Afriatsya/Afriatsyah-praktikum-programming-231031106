6af267c3ce9f27db4bf22234104e7a3cc67651d0
Afriatsya <153706658+Afriatsya@users.noreply.github.com>
2023-12-18 20:20:51 +0800

    upload file 14

---
+++ 231031106-Afriatsyah Nasra Perdana-percabangan3.py
@@ -0,0 +1,12 @@
+nama = input("Masukkan nama karyawan: ")
+pendapatan = input("Masukkan besaran pendapatan: ")
+pendapatan = int(pendapatan)
+
+print("Nama karyawan:", nama)
+
+print("Pendapatan karyawan:", pendapatan)
+
+if pendapatan > 1000:
+    print("Status: Berhak")
+else:
+    print("Status: Tidak Berhak")
